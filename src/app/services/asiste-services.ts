13e9c0cdfdff3fa072e9f49b4de9027b45350938
Johnny Alexander Gutierrez Rendon <Jgutierrezr@intergrupo.com>
2020-07-16 15:24:04 -0500

    Dist

--- src/app/services/asiste-services.ts
+++ src/app/services/asiste-services.ts
@@ -15,6 +15,6 @@ constructor(private httpClient: HttpClient) {
 }
 
 public reloadLocation(id: string): Observable<any>{
-    return this.httpClient.get(`/api/AsistePlatform/UbicationLinkAPI.jsn?id=${id}`, {responseType: 'json'});
+    return this.httpClient.get(`http://asisteservicewebapi-dev.us-west-2.elasticbeanstalk.com/api/Service/statusService?id=${id}`, {responseType: 'json'});
     }
  }
