648e52d01c178b50688f5cd6af29f8a4cf570c75
Johnny Alexander Gutierrez Rendon <Jgutierrezr@intergrupo.com>
2020-10-21 23:46:40 -0500

    update endpoints

--- src/app/services/asiste-services.ts
+++ src/app/services/asiste-services.ts
@@ -15,12 +15,12 @@ constructor(private httpClient: HttpClient) {
 }
 
 public reloadLocation(id: string): Observable<any>{
-    return this.httpClient.get(`http://localhost:8080/AsistePlatform/UbicationLinkAPI.jsn?id=${id}`, {responseType: 'json'});
-    // return this.httpClient.get(`https://plataforma.asisteapp.co/AsistePlatform/UbicationLinkAPI.jsn?id=${id}`, {responseType: 'json'});
+    // return this.httpClient.get(`http://localhost:8080/AsistePlatform/UbicationLinkAPI.jsn?id=${id}`, {responseType: 'json'});
+    return this.httpClient.get(`https://plataforma.asisteapp.co/AsistePlatform/UbicationLinkAPI.jsn?id=${id}`, {responseType: 'json'});
     }
 
  public sendRating(id: string, calificacion: number, comentario: string): Observable<any>{
     // return this.httpClient.get(`http://localhost:8080/AsistePlatform/UbicationLinkAPI.jsn?id=${id}`, {responseType: 'json'});
-    return this.httpClient.get(`http://localhost:8080/AsistePlatform/SaveCalificacion.jsn?id=${id}&calificacion=${calificacion}&comentario=${comentario}`, {responseType: 'json'});
+    return this.httpClient.get(`https://plataforma.asisteapp.co/AsistePlatform/SaveCalificacion.jsn?id=${id}&calificacion=${calificacion}&comentario=${comentario}`, {responseType: 'json'});
     }
  }
